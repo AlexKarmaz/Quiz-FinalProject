670e0ee15788754c225de743f0443dd43486c8ee
AlexKarmaz <karmazct@mail.ru>
2017-07-31 17:17:31 +0300

    Fixed test passing

--- PLMVC/Controllers/ErrorController.cs
+++ PLMVC/Controllers/ErrorController.cs
@@ -9,30 +9,30 @@ namespace PLMVC.Controllers
 {
     public class ErrorController : Controller
     {
-        private readonly ILogger logger;
+       // private readonly ILogger logger;
 
-        public ErrorController(ILogger logger)
-        {
-            this.logger = logger;
-        }
+        //public ErrorController(ILogger logger)
+        //{
+        //    this.logger = logger;
+        //}
         public ActionResult NotFound()
         {
             Response.StatusCode = 404;
-            logger.Error("Not Found error 404");
+           // logger.Error("Not Found error 404");
             return View();
         }
 
         public ActionResult Error()
         {
             Response.StatusCode = 400;
-            logger.Error("Bad request error 400");
+           // logger.Error("Bad request error 400");
             return View();
         }
 
         public ActionResult ServerError()
         {
             Response.StatusCode = 500;
-            logger.Error("Server error 500");
+          //  logger.Error("Server error 500");
             return View();
         }
     }
