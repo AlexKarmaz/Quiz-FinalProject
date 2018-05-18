6f5c386a414c992d4df5d753e5dcc8ff2d5f0a32
AlexKarmaz <karmazct@mail.ru>
2018-05-18 03:04:34 +0300

    add localization

--- PLMVC/Controllers/HomeController.cs
+++ PLMVC/Controllers/HomeController.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PLMVC.Filters;
 
 namespace PLMVC.Controllers
 {
+    [Culture]
     public class HomeController : Controller
     {
         private readonly IUserService userService;
@@ -18,8 +20,24 @@ namespace PLMVC.Controllers
             this.userService = userService;
         }
 
-        
-        
+        [AllowAnonymous]
+        public ActionResult ChangeCulture(string lang)
+        {
+            List<string> cultures = new List<string>() { "ru", "en" };
+            lang = !cultures.Contains(lang) ? "ru" : lang;
+            HttpCookie cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;
+            else
+            {
+                cookie = new HttpCookie("lang", lang);
+                cookie.HttpOnly = false;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(Request.UrlReferrer.AbsolutePath);
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ PLMVC/Filters/CultureAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PLMVC.Filters
+{
+    public class CultureAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+           
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string cultureName = null;
+
+            HttpCookie cultureCookie = filterContext.HttpContext.Request.Cookies["lang"];
+            if (cultureCookie != null)
+                cultureName = cultureCookie.Value;
+            else
+                cultureName = "ru";
+
+            List<string> cultures = new List<string>() { "ru", "en" };
+            if (!cultures.Contains(cultureName))
+            {
+                cultureName = "ru";
+            }
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureName);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(cultureName);
+        }
+    }
+}
